27a3226c850d7b02965efc8793a9fd0c713486d3
Agaba Andrew <agabaandre@gmail.com>
2024-10-15 18:10:27 +0300

    new tabs

--- application/modules/records/views/dashboard.php
+++ application/modules/records/views/dashboard.php
@@ -4,7 +4,9 @@
         <div class="col-12">
             <div class="embed-responsive embed-responsive-16by9" style="height: 100vh; margin-top:10px; margin-bottom:10px;">
                 <iframe class="embed-responsive-item"
-                    src="<?php echo $active_url ?>"
+                    src="<?php 
+                    $no_data =base_url().'error/error.html';
+                    if($active_url!='#'){ echo $active_url;}else{ echo ""} ?>"
                     style="border:0;" allowfullscreen>
                 </iframe>
             </div>
